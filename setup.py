43a176fa6696fc20cf22ac5d24b824dbf6fba3df
pgoel05 <prateekgoyal29@gmail.com>
2024-03-29 02:33:43 +0530

    Installed Requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "mcqgenerator",
+    version = "0.0.1",
+    author = "Prateek Goel",
+    install_requires = ["langchain", "pandas", "PyPDF2", "openai", "streamlit", "python-dotenv"],
+    packages = find_packages()
+)
